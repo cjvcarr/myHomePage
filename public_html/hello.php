c2212b291b94e265962d5808972bca3ec35e93bf
Christopher Carr <ccarr@itcollege.ee>
2016-09-19 17:36:23 +0300

    Added public_html

---
+++ public_html/hello.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+  <script type="text/javascript" src="hello.js">
+    Your browser does not support Javascript, upgrade man!
+  </script>
+</head>
+<body>
+The rest is actually HTML
+<?php
+
+#This is hello world in PHP
+#This file is saved to public_html as hello.php
+
+echo "Hello, world!";
+echo "Today is " . date("l");
+
+#to run open
+#http://enos.itcollege.ee/~ccarr/hello.php
+
+#phpinfo();
